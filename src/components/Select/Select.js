1fcf4462c5dc1ae2f4feb9940e3e307420ead389
Paul Nogas <Paul.Nogas@soti.net>
2021-04-29 16:49:47 -0400

    Step2: Code along
    Very interesting trick with duplication of a custom element and a native
    element that is invisible. Let the native element handle the events, and
    the custom element handle the style.
    
    Also interesting with the adjacent sibling combination. How to have a
    hover on a parent impact the child!
    + for direct sibling (only when nothing between)
    ~ for anywhere down the sibling path (no matter what's in between)
    
    IconWrapper technique is okay.
    
    Glad I watched instead of struggling too much on my own.

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -7,12 +7,62 @@ import { getDisplayedValue } from './Select.helpers';
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
+  const size = 24;
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationSelect>
+        {displayedValue} 
+        <IconWrapper style={{"--size": size + "px"}}>
+          <Icon id="chevron-down" strokeWidth={1} size={size}/>
+        </IconWrapper>
+      </PresentationSelect>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 16px;
+  margin: auto;
+  width: var(--size);
+  height: var(--size);
+  pointer-events: none;
+`;
+const PresentationSelect = styled.div`
+  border-radius: 8px;
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  font-size: ${16/16}rem;
+  padding: 12px 16px;
+  padding-right: 52px;
+
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color; 
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
 
 export default Select;
